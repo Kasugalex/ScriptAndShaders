5cc07cd381db853af61b7f50cc921b659668b399
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-04-19 18:04:57 +0800

    增加后处理

---
+++ Assets/Shader/KiMosaic/MySelfThink/KiMosaicTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KiMosaicTest : PostEffectBase
+{
+
+    [Range(1, 50)]
+    public int level = 10;
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            material.SetFloat("_Level", level);
+            Graphics.Blit(src, dest, material);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+
+}
