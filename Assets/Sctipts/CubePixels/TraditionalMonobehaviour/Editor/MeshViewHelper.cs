13253e90f7b72d68bb1b49e0515279536b8612e7
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-04-23 11:20:37 +0800

    CreateCubeMeshAndUV

---
+++ Assets/Sctipts/CubePixels/TraditionalMonobehaviour/Editor/MeshViewHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+
+[CustomEditor(typeof(MeshViewer))]
+public class MeshViewHelper : Editor
+{
+
+    private void OnSceneGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.red;
+        MeshViewer viewer = target as MeshViewer;
+        Dictionary<Vector3, StringBuilder> posList = new Dictionary<Vector3, StringBuilder>();
+
+        for (int i = 0, max = viewer.verticesList.Count; i < max; i++)
+        {
+            Vector3 vPos = viewer.transform.TransformPoint(viewer.verticesList[i]);
+
+            StringBuilder sb;
+            if (posList.TryGetValue(vPos, out sb))
+            {
+                sb.AppendLine("index:" + i);
+            }
+            else
+            {
+                sb = new StringBuilder();
+                sb.AppendLine("index" + i);
+                posList.Add(vPos, sb);
+
+            }
+
+            Handles.Label(vPos, sb.ToString(), style);
+        }
+    }
+
+}
---
+++ Assets/Sctipts/CubePixels/TraditionalMonobehaviour/MeshViewer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshViewer : MonoBehaviour {
+    [HideInInspector]
+    public List<Vector3> verticesList = new List<Vector3>();
+    [SerializeField]
+    private List<Vector2> uvList = new List<Vector2>();
+    [SerializeField]
+    private List<int> triList = new List<int>();
+
+    public List<Vector3> verticesWorldPosList = new List<Vector3>();
+
+    private Transform trans;
+    private void Start()
+    {
+        trans = transform;
+        ReadMeshInfo();
+    }
+
+    private Vector3 lastPos = Vector3.zero;
+    private void Update()
+    {
+        if(trans.position != lastPos)
+        {
+            Vector3 offset = trans.position - lastPos;
+            lastPos = trans.position;
+            for (int i = 0; i < verticesWorldPosList.Count; i++)
+            {
+                verticesWorldPosList[i] += offset;
+            }
+        }
+    }
+
+    private void ReadMeshInfo()
+    {
+        MeshFilter _meshFilter = GetComponent<MeshFilter>();
+        Mesh _mesh = _meshFilter.mesh;
+
+        for (int i = 0, imax = _mesh.vertexCount; i < imax; ++i)
+        {
+            verticesList.Add(_mesh.vertices[i]);
+            uvList.Add(_mesh.uv[i]);
+        }
+
+        for (int i = 0, imax = _mesh.triangles.Length; i < imax; ++i)
+        {
+            triList.Add(_mesh.triangles[i]);
+        }
+
+        verticesWorldPosList.AddRange(verticesList);
+    }
+}
