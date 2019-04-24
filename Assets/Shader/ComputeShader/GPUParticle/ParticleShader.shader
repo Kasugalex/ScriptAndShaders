dfa2e26748a4b12717af2bd018b95e6df513cd60
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-04-24 09:23:59 +0800

    CPU Particle

---
+++ Assets/Shader/ComputeShader/GPUParticle/ParticleShader.shader
@@ -0,0 +1,47 @@
+﻿Shader "Unlit/ParticleShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+		SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		LOD 200
+
+		Pass
+		{
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag				
+			#include "UnityCG.cginc"
+
+			StructuredBuffer<float3> buf_Points;
+			StructuredBuffer<float3> buf_Colors;
+
+			struct ps_input {
+				float4 pos:SV_POSITION;
+				half3 color:COLOR;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			ps_input vert (uint id :SV_VertexID)
+			{
+				ps_input o;
+				float3 pos = buf_Points[id]; //存储点的位置，buf_Points在compute shader已放置了点位置
+				o.color = buf_Colors[id];
+				o.pos = mul(UNITY_MATRIX_VP, float4(pos, 1));
+				return o;
+			}
+			
+			fixed4 frag (ps_input i) : SV_Target
+			{
+				return fixed4(i.color,0.5);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Shader/ComputeShader/GPUParticle/SetProperty.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetProperty : MonoBehaviour {
+
+    public Shader shader;
+    public ComputeShader computeShader;
+    private ComputeBuffer offsetBuffer;
+    private ComputeBuffer outputBuffer;
+    private ComputeBuffer constantBuffer;
+    private ComputeBuffer colorBuffer;
+
+    private Material material;
+    private int kernel;
+    public const int vertexCount = 65536; //64*64*4
+
+    void Start () {
+        CreateBuffers();
+        material = new Material(shader);
+        kernel = computeShader.FindKernel("CSMain");
+	}
+
+    private void OnDisable()
+    {
+        ReleaseBuffer();
+    }
+
+    private void OnPostRender()
+    {
+        Dispatch();
+
+        material.SetPass(0);
+        material.SetBuffer("buf_Points", outputBuffer);
+        material.SetBuffer("buf_Colors", colorBuffer);
+        Graphics.DrawProcedural(MeshTopology.Points, vertexCount);
+    }
+
+    private void Dispatch()
+    {
+        constantBuffer.SetData(new[] { Time.time });
+
+        computeShader.SetBuffer(kernel,"cBuffer",constantBuffer);
+        computeShader.SetBuffer(kernel,"offsets",offsetBuffer);
+        computeShader.SetBuffer(kernel,"output",outputBuffer);
+        computeShader.SetBuffer(kernel,"color",colorBuffer);
+        computeShader.Dispatch(kernel, 64, 64, 1);
+        
+    }
+
+    private void CreateBuffers()
+    {
+        offsetBuffer = new ComputeBuffer(vertexCount, 4);
+        float[] values = new float[vertexCount];
+
+        for (int i = 0; i < vertexCount; i++)
+        {
+            values[i] = UnityEngine.Random.value * 2 * Mathf.PI;
+        }
+        offsetBuffer.SetData(values);
+
+        constantBuffer = new ComputeBuffer(1, 4);
+        colorBuffer = new ComputeBuffer(vertexCount, 12);
+        outputBuffer = new ComputeBuffer(vertexCount,12);
+
+    }
+
+    private void ReleaseBuffer()
+    {
+        constantBuffer.Release();
+        offsetBuffer.Release();
+        outputBuffer.Release();
+
+        DestroyImmediate(material);
+    }
+
+
+
+}
