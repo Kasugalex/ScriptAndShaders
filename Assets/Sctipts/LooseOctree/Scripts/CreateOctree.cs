e7aa92b503d512f015b1438ac40edfb2a5162aec
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-04-25 18:51:12 +0800

    Update

---
+++ Assets/Sctipts/LooseOctree/Scripts/CreateOctree.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateOctree : MonoBehaviour {
+    BoundsOctree<GameObject> boundsTree;
+
+    public Transform container;
+
+    private List<GameObject> objList;
+    void Start()
+    {
+
+      
+
+    }
+
+    void Update()
+    {
+        boundsTree = new BoundsOctree<GameObject>(15, container.position, 1, 1.25f);
+        foreach (Transform item in container)
+        {
+            boundsTree.Add(item.gameObject, new Bounds(item.position, Vector3.one));
+        }
+
+    }
+
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        boundsTree.DrawAllBounds();
+        boundsTree.DrawAllObjects();
+
+        boundsTree.DrawCollisionChecks();
+    }
+
+#endif
+
+}
