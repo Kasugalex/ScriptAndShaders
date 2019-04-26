756a5e9d1d9b248a9d571be8349183b971508efc
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-04-26 12:05:04 +0800

    Update

---
+++ Assets/Shader/InteractiveWater/InteractiveWater.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveWater : MonoBehaviour {
+
+
+    public Texture waterTexture;
+
+    public Texture noiseTex;
+
+    public Shader waterShader;
+
+    [Space(15)]
+    public float WindForce = 10;
+
+    public Vector3 WindDirection = Vector3.zero;
+
+    private Transform _trans;
+
+    private Material waterMaterial;
+
+    private MeshRenderer waterMesh;
+	void Start () {
+        _trans = transform;
+        WindDirection = _trans.forward.normalized;
+
+        waterMaterial = new Material(waterShader);
+        waterMesh = FindObjectOfType<MeshRenderer>();
+        waterMaterial.mainTexture = waterTexture;
+        waterMaterial.SetTexture("_NoiseTex", noiseTex);
+        waterMesh.material = waterMaterial;
+    }
+
+    private Vector3 lastDirection;
+    private float lastForce;
+    private void Update()
+    {
+
+        WindDirection = _trans.forward.normalized;
+        _trans.eulerAngles = new Vector3(0, _trans.eulerAngles.y, 0);
+
+        if (lastDirection == WindDirection && WindForce == lastForce) return;
+        lastDirection = WindDirection;
+        lastForce = WindForce;
+
+        SetShaderProperty();
+    }
+
+
+    private void SetShaderProperty()
+    {
+
+        //waterMaterial.SetFloat("_WaveFrequency", _WaveFrequency);
+        waterMaterial.SetFloat("_WindForce", WindForce);
+        waterMaterial.SetVector("_Direction", new Vector2(WindDirection.x,WindDirection.z));
+
+        if(WindDirection.x != 0)
+        {
+            int noisexScroll = WindDirection.x > 0 ? 1 : -1;
+            waterMaterial.SetFloat("_NoisexScroll", noisexScroll);
+        }
+
+        if (WindDirection.y != 0)
+        {
+            int noiseyScroll = WindDirection.y > 0 ? 1 : -1;
+            waterMaterial.SetFloat("_NoiseyScroll", noiseyScroll);
+        }
+    }
+
+}
---
+++ Assets/Shader/InteractiveWater/InteractiveWater.shader
@@ -0,0 +1,61 @@
+﻿Shader "Unlit/InteractiveWater"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _NoiseTex;
+
+			float _WindForce;
+
+			float2 _Direction;
+
+			fixed _NoisexScroll;
+			fixed _NoiseyScroll;
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				o.uv += float2 (_Direction.x, _Direction.y) * _Time.x * _WindForce;
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{		
+				fixed4 noise = tex2D(_NoiseTex,fixed2(i.uv.x + _Time.x * _NoisexScroll ,i.uv.y + _Time.x * _NoiseyScroll));
+				fixed4 col = tex2D(_MainTex, i.uv + noise.xy * min(_WindForce,0.25));
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
