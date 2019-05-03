ab87a945b87cfb7d845063fb2588796effd88c94
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-05-03 10:53:45 +0800

    Update

--- Assets/Sctipts/SortAlgorithm/SortAlgorithm.cs
+++ Assets/Sctipts/SortAlgorithm/SortAlgorithm.cs
@@ -6,9 +6,10 @@ public class SortAlgorithm
 {
 
     #region  快排
-    public static void QuickSort(int[] array,int low,int high)
+    public static void QuickSort(int[] array, int low, int high)
     {
-		if(low >= high)
+
+        if (low >= high)
             return;
         int index = SplitArray(array, low, high);
         QuickSort(array, low, index - 1);
@@ -23,11 +24,11 @@ public class SortAlgorithm
         {
             while (array[high] >= key && low < high)
                 --high;
-			//比key小的放左边
+            //比key小的放左边
             array[low] = array[high];
             while (array[low] <= key && low < high)
                 ++low;
-			//比key大的放右边
+            //比key大的放右边
             array[high] = array[low];
         }
         array[low] = key;
@@ -36,4 +37,37 @@ public class SortAlgorithm
     }
     #endregion
 
+    #region 希尔排序
+
+    public static void ShellSort(int[] array)
+    {
+        int length = array.Length;
+        int _d = array.Length;
+        for (_d = _d / 2; _d > 0;)
+        {
+            if (_d % 2 == 0) _d++;
+            for (int i = 0; i < _d; i++)
+            {
+                for (int j = i + _d; j < length; j++)
+                {
+                    if (j < length)
+                    {
+                        if (array[j] < array[j - _d])
+                        {
+                            int temp = array[j];
+                            int k = 0;
+                            for (k = j - _d; k >= i && temp < array[k]; k -= _d)
+                            {
+                                array[k + _d] = array[k];
+                            }
+                            array[k + _d] = temp;
+                        }
+                    }
+                }
+            }
+            _d = _d / 2;
+        }
+    }
+
+    #endregion
 }
---
+++ Assets/Sctipts/SortAlgorithm/SortTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class SortTest : MonoBehaviour
+{
+    public int ArrayLength = 50;
+    [Tooltip("数组值的绝对值范围")]
+    public int ValueAbsolute = 30;
+    [SerializeField]
+    private int[] Array;
+    void Start()
+    {
+        GenerateArray();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Sort();
+        }
+    }
+
+
+    private void GenerateArray()
+    {
+        Array = null;
+        Array = new int[ArrayLength];
+        ValueAbsolute = ValueAbsolute < 0 ? -ValueAbsolute : ValueAbsolute;
+        for (int i = 0; i < ArrayLength; i++)
+        {
+            int random = Random.Range(-ValueAbsolute, ValueAbsolute);
+            Array[i] = random;
+        }
+    }
+    private void Sort()
+    {
+        WatchExecuteTime.WatchExecute(ShellSort);
+    }
+    private void ShellSort()
+    {
+        SortAlgorithm.ShellSort(Array);
+    }
+
+
+
+ 	private void OnGUI() {
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.white;
+        style.fontSize = 40;
+        int width = 500;
+        GUI.TextArea(new Rect((Screen.width >> 1) - width / 2, 100, width, 200), "运行后按下鼠标左键进行排序",style);
+    }
+}
