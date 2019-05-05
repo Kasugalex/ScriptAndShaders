3392a5a01f16b5c58e1c4c7e5121395dd760229b
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-05-05 18:41:34 +0800

    Update

---
+++ Assets/Sctipts/Jobs/1.Test/JobTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+
+public class JobTest : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+}
