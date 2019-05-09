ee7b340e4024a989fc551ac45f8f0bf3c0938800
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-05-09 11:41:56 +0800

    Update

---
+++ Assets/Sctipts/Jobs/1.SimpleTest/SimpleJobs.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using Unity.Collections;
+public class SimpleJobs : MonoBehaviour
+{
+
+    void Start()
+    {
+        NativeArray<float> result = new NativeArray<float>(1, Allocator.TempJob);
+
+        MyJob jobData = new MyJob();
+        jobData.a = 10;
+        jobData.b = 10;
+        jobData.result = result;
+        //执行job1
+        JobHandle handle = jobData.Schedule();
+
+        //设置job2
+        AddOneJob secJobData = new AddOneJob();
+        secJobData.result = result;
+        //执行job2，传入job1
+        JobHandle secHandle = secJobData.Schedule(handle);
+        //等待job2完成
+        secHandle.Complete();
+
+        float aPlusB = result[0];
+        //释放内存
+        result.Dispose();
+
+        Debug.Log(aPlusB);
+    }
+
+}
+
+struct MyJob : IJob
+{
+    public float a;
+    public float b;
+    public NativeArray<float> result;
+
+    public void Execute()
+    {
+        result[0] = a + b;
+    }
+}
+
+struct AddOneJob : IJob
+{
+    public NativeArray<float> result;
+    public void Execute()
+    {
+        result[0] = result[0] + 1;
+    }
+}
---
+++ Assets/Sctipts/Jobs/2.ParallelJobs/ParallelJobs.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using Unity.Collections;
+
+public class ParallelJobs : MonoBehaviour
+{
+    void Start()
+    {
+        NativeArray<float> a = new NativeArray<float>(2, Allocator.TempJob);
+        NativeArray<float> b = new NativeArray<float>(2, Allocator.TempJob);
+        NativeArray<float> result = new NativeArray<float>(2, Allocator.TempJob);
+
+        a[0] = 1.1f;
+        b[0] = 2.2f;
+        a[1] = 3.3f;
+        b[1] = 4.4f;
+
+        MyParallelJob jobData = new MyParallelJob();
+        jobData.a = a;
+        jobData.b = b;
+        jobData.result = result;
+
+        //执行当前Job
+        JobHandle handle = jobData.Schedule(result.Length, 1);
+
+        handle.Complete();
+
+        a.Dispose();
+        b.Dispose();
+        result.Dispose();
+    }
+
+}
+
+struct IncrementByDeltaTimeJob : IJobParallelFor
+{
+    public NativeArray<float> values;
+    public float deltaTime;
+    public void Execute(int index)
+    {
+        float temp = values[index];
+        temp += deltaTime;
+        values[index] = temp;
+    }
+}
+
+struct MyParallelJob : IJobParallelFor
+{
+    [ReadOnly]
+    public NativeArray<float> a;
+
+    [ReadOnly]
+    public NativeArray<float> b;
+
+    public NativeArray<float> result;
+
+    public void Execute(int index)
+    {
+        result[index] = a[index] + b[index];
+        Debug.Log(index + "|" + result[index]);
+    }
+}
