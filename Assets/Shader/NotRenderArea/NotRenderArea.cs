9d86e362f4b48c5eac469ec9b27b27920f291827
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-05-10 10:42:13 +0800

    Update

---
+++ Assets/Shader/NotRenderArea/NotRenderArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class NotRenderArea : PostEffectBase
+{
+    [Range(0.0f,1.0f)]
+    public float Y;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(material != null)
+        {
+            material.SetFloat("Y", Y);
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
