9a70f863aa8f1438af40bcd41dbe38a0bee24b2a
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-05-24 14:21:03 +0800

    Update

--- Assets/Shader/ComputeShader/4.Tex2DColorCube/Tex2DColorSystem.cs
+++ Assets/Shader/ComputeShader/4.Tex2DColorCube/Tex2DColorSystem.cs
@@ -6,57 +6,34 @@ using UnityEngine;
 
 public class Tex2DColorSystem : MonoBehaviour {
 
-    [SerializeField] int                    edgeLength                  = 100;
-    [SerializeField] Shader                 particleShader;
-    [SerializeField] ComputeShader          updateShader;
-    [SerializeField, Range (0.0f, 1.0f)]    float deceleration          = 1f;
+    [SerializeField] int edgeLength = 100;
+    [SerializeField] Material particleMaterial;
+    [SerializeField] ComputeShader updateShader;
+    [SerializeField, Range (0.0f, 1.0f)] float deceleration = 1f;
 
-    private const int                       _Thred                      = 8;
-
+    private const int _Thred = 8;
+    private Transform _trans;
+    private MaterialPropertyBlock block;
+    public Mesh m;
     private void Start () {
-
+        _trans = transform;
         GenerateParticles ();
     }
 
     private void Update () {
 
+        Graphics.DrawMesh (SimpleCubeMesh(1,0), _trans.localToWorldMatrix, particleMaterial, 0, null, 0, block);
+        Graphics.DrawMesh (m, Vector3.one, Quaternion.identity, particleMaterial, 0, null, 0, block);
+
     }
 
     private void GenerateParticles () {
-
+        
     }
 
-    private Mesh BuildMesh (int count, int xStart, int yStart) {
-
-        Mesh particleMesh = new Mesh ();
-        particleMesh.name = count.ToString ();
-
-        var dcount = count * count;
-        var tcount = dcount * count;
-
-        var scale = (1f / count);
-        var dscale = scale * scale;
-        var offset = -Vector3.one * 0.5f;
-
-        var vertices = new Vector3[tcount];
-        var uvs = new Vector2[tcount];
-        var indices = new int[tcount];
-
-        for (int x = 0; x < count; x++) {
-            var xoffset = x * dcount;
-            for (int y = 0; y < count; y++) {
-                var yoffset = y * count;
-
-                var index = xoffset + yoffset;
-                vertices[index] = new Vector3 (x + xStart, y + yStart, 0) * scale + offset;
-                uvs[index] = new Vector2 (x * scale, y * scale);
-                indices[index] = index;
-            }
-        }
-
-        particleMesh.vertices = vertices;
-        particleMesh.SetIndices (indices, MeshTopology.Points, 0);
-        return particleMesh;
+    private Mesh SimpleCubeMesh(int xOffset,int yOffset){
+        Mesh mesh = CreateMesh.CreateCubeMesh(xOffset,yOffset,0);
+        return mesh;
     }
 
     private void OnDisable () {
