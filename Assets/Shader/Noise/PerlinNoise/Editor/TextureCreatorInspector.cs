9bc65906931ca6e9e7cadbdd093070ce9a7a4b35
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-05-31 10:52:05 +0800

    Update

---
+++ Assets/Shader/Noise/PerlinNoise/Editor/TextureCreatorInspector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(TextureCreator))]
+public class TextureCreatorInspector : Editor
+{
+    private TextureCreator creator;
+
+    private void OnEnable()
+    {
+        creator = target as TextureCreator;
+        Undo.undoRedoPerformed += RefreshCreator;
+    }
+
+    private void OnDisable()
+    {
+        Undo.undoRedoPerformed -= RefreshCreator;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        if(EditorGUI.EndChangeCheck())
+        {
+            RefreshCreator();
+        }
+    }
+
+    private void RefreshCreator()
+    {
+        if (Application.isPlaying)
+            creator.FillTexture();
+    }
+}
---
+++ Assets/Shader/Noise/PerlinNoise/TextureCreator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureCreator : MonoBehaviour
+{
+    [Range(2,512)]
+    public int resolution = 256;
+    private Texture2D texture;
+
+    private void OnEnable()
+    {
+        if (texture == null)
+        {
+            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, false);
+            texture.name = "Procedural Texture";
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Bilinear;
+            texture.anisoLevel = 9;
+            GetComponent<MeshRenderer>().material.mainTexture = texture;
+        }
+        FillTexture();
+    }
+
+    public void FillTexture()
+    {
+        if(texture.width != resolution)
+        {
+            texture.Resize(resolution, resolution);
+        }
+
+        Vector3 point00 = new Vector3(-0.5f, -0.5f);
+        Vector3 point10 = new Vector3(0.5f, -0.5f);
+        Vector3 point01 = new Vector3(-0.5f, 0.5f);
+        Vector3 point11 = new Vector3(0.5f, 0.5f);
+
+        float stepSize = 1f / resolution;
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                texture.SetPixel(x, y, new Color((x + 0.5f) * stepSize % 0.1f,(y + 0.5f) * stepSize % 0.1f,0f) * 10f);
+            }
+        }
+
+        texture.Apply();
+    }
+}
